dd0dd4941f491dbc4988d4e0f0bb848b6097b58d
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-09-14 03:32:07 +0100

    Remove Prettier configuration and clean up event list formatting

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -1,26 +1,23 @@
 export const events = [
-//add on top new events   
-            {
+//add on top new events
+    {
         imageUrl: '/images/posts/bootcampOrange.jpg',
         eventName: 'Flutter Mobile Development with Orange',
         category: 'Bootcamp'
     },
-            {
+    {
         imageUrl: '/images/posts/bootcampSchedule.jpg',
         eventName: 'WIE ACT Bootcamp',
         category: 'Announcement'
     },
-
-        {
+{
         imageUrl: '/images/posts/nonTechInfo.jpg',
         eventName: 'Non Tech Challenge Info Session',
         category: 'Info Session'
     },
-
-            {
+{
         imageUrl: '/images/posts/non-tech-chall.jpg',
         eventName: 'Non Tech Challenge Extended',
         category: 'Extension'
-    },
-
+    }
 ];
