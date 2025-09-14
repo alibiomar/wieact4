2842165ecfd06f16f82f6fad10b4c2a7a68679c5
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-09-14 16:39:28 +0100

    Add new event for 'AI Applications in Agriculture' to events list

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -1,6 +1,11 @@
 export const events = [
 //add on top new events
     {
+        imageUrl: '/images/posts/bootcampAmal.jpg',
+        eventName: 'AI Applications in Agriculture',
+        category: 'Bootcamp'
+    },
+    {
         imageUrl: '/images/posts/bootcampOrange.jpg',
         eventName: 'Flutter Mobile Development with Orange',
         category: 'Bootcamp'
@@ -10,19 +15,15 @@ export const events = [
         eventName: 'WIE ACT Bootcamp',
         category: 'Announcement'
     },
-{
+    {
         imageUrl: '/images/posts/nonTechInfo.jpg',
         eventName: 'Non Tech Challenge Info Session',
         category: 'Info Session'
     },
-{
+    {
         imageUrl: '/images/posts/non-tech-chall.jpg',
         eventName: 'Non Tech Challenge Extended',
         category: 'Extension'
     },
-        {
-        imageUrl: '/images/posts/bootcampAmal.jpg',
-        eventName: 'AI Applications in Agriculture',
-        category: 'Bootcamp'
-    }
+
 ];
